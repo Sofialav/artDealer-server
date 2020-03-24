987e9e11bbda51edc6214340da18b423e5735939
Sofialav <cemeteryvixen@gmail.com>
2020-03-24 18:48:27 +0100

    Add Artwork db model

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const artist = require("./artist/model");
+const artwork = require("./artwork/model");
 
 const port = process.env.PORT || 4000;
 const app = express();
