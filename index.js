407233d5792370b4bfee9d85a7dc12a7446cb08c
Sofialav <cemeteryvixen@gmail.com>
2020-03-25 12:10:13 +0100

    Add post/artforms, get/artforms

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const materialRouter = require("./material/router");
 const genreRouter = require("./genre/router");
+const artFormRouter = require("./artForm/router");
 const artist = require("./artist/model");
 const artwork = require("./artwork/model");
-const artForm = require("./artForm/model");
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -15,5 +15,6 @@ app.use(corsMW);
 app.use(parserMW);
 app.use(materialRouter);
 app.use(genreRouter);
+app.use(artFormRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
