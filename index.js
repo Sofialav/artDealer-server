66e676531f3143f55666d38d42562f943a1710e9
Sofialav <cemeteryvixen@gmail.com>
2020-03-25 12:33:41 +0100

    Preset artist & artwork routers

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require("cors");
 const materialRouter = require("./material/router");
 const genreRouter = require("./genre/router");
 const artFormRouter = require("./artForm/router");
-const artist = require("./artist/model");
-const artwork = require("./artwork/model");
+const artistRouter = require("./artist/router");
+const artworkRouter = require("./artwork/router");
 
 const port = process.env.PORT || 4000;
 const app = express();
@@ -16,5 +16,7 @@ app.use(parserMW);
 app.use(materialRouter);
 app.use(genreRouter);
 app.use(artFormRouter);
+app.use(artistRouter);
+app.use(artworkRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
