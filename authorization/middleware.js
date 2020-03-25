ae8b9cef973b98924cb5237172c4b1f41467aa9e
Sofialav <cemeteryvixen@gmail.com>
2020-03-25 14:27:33 +0100

    Add auth middleware, login router

---
+++ authorization/middleware.js
@@ -0,0 +1,34 @@
+const { toData } = require("./jwt");
+const Artist = require("../artist/model");
+const Artwork = require("../artwork/model");
+
+function auth(req, res, next) {
+  const auth =
+    req.headers.authorization && req.headers.authorization.split(" ");
+
+  if (auth && auth[0] === "Bearer" && auth[1]) {
+    try {
+      const data = toData(auth[1]);
+      Artist.findByPk(data.artistId, {
+        attributes: ["login", "id"]
+        // include: [Artwork]
+      })
+        .then(artist => {
+          if (!artist) return next("Artist does not exist");
+          req.artist = artist;
+          next();
+        })
+        .catch(next);
+    } catch (error) {
+      res.status(400).send({
+        message: `Error ${error.name}: ${error.message}`
+      });
+    }
+  } else {
+    res.status(401).send({
+      message: "Please supply some valid credentials"
+    });
+  }
+}
+
+module.exports = auth;
