67d8f4cc3adcaf622733d1dfe38f7665e503f379
Sofialav <cemeteryvixen@gmail.com>
2020-03-25 12:02:45 +0100

    Add post/genres, get/genres

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const materialRouter = require("./material/router");
+const genreRouter = require("./genre/router");
 const artist = require("./artist/model");
 const artwork = require("./artwork/model");
-const genre = require("./genre/model");
 const artForm = require("./artForm/model");
 
 const port = process.env.PORT || 4000;
@@ -14,5 +14,6 @@ const parserMW = express.json();
 app.use(corsMW);
 app.use(parserMW);
 app.use(materialRouter);
+app.use(genreRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- material/router.js
+++ material/router.js
@@ -27,4 +27,5 @@ router.get("/materials", async (req, res, next) => {
     next(error);
   }
 });
+
 module.exports = router;
